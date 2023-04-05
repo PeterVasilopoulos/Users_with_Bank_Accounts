712105fd23ed5e93af6ed7a5ab918028f6ec86c7
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-04-05 13:22:30 -0400

    Copied bank account class from another file

--- users_with_bank_accounts.py
+++ users_with_bank_accounts.py
@@ -11,4 +11,31 @@ class User:
 
 # Bank Account Class
 class BankAccount:
-    pass
+    def __init__(self, int_rate = 0.01, balance = 0):
+        # Assign attribute values
+        self.int_rate = int_rate
+        self.balance = balance
+    
+    # Deposit 
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    # Withdraw
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("Insuffiencet funds: Charging a $5 fee")
+            self.balance -= amount
+        return self
+
+    # Display account info
+    def display_account_info(self):
+        print(f"Interest Rate: {self.int_rate}, Balance: {self.balance}")
+        return self
+
+    # Yield interest
+    def yield_interest(self):
+        self.balance += self.balance * self.int_rate
+        return self
