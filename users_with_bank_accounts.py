bef10768f210c456c999cbd9de47b3339cb47041
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-04-05 13:30:39 -0400

    Completed main requirements

--- users_with_bank_accounts.py
+++ users_with_bank_accounts.py
@@ -1,12 +1,26 @@
 # User Class
 class User:
     def __init__(self, name, email):
+        # Assign attribute values
         self.name = name
         self.email = email 
+        # Create instance of bank account
         self.account = BankAccount(int_rate = 0.02, balance = 0)
 
+    # Make Deposit
     def make_deposit(self, amount):
-        pass 
+        self.account.deposit(amount)
+        return self
+
+    # Make Withdrawal
+    def make_withdrawal(self, amount):
+        self.account.withdraw(amount)
+        return self
+
+    # Display user balance
+    def display_user_balance(self):
+        self.account.display_account_info()
+        return self
 
 
 # Bank Account Class
@@ -39,3 +53,9 @@ class BankAccount:
     def yield_interest(self):
         self.balance += self.balance * self.int_rate
         return self
+
+
+pog = User("K", "k@mail.com")
+pog.make_deposit(50)
+pog.make_withdrawal(20)
+pog.display_user_balance()
